274152b0c3725de7a42ac55242424442d45d6445
rommy zohara <rommy_zohara@yahoo.com>
2022-12-19 07:29:53 +0700

    setting vue + router

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            vue
+        </div>
+        <button @click="$router.push('/')">home</button>
+        <button @click="$router.push('/about')">about</button>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
